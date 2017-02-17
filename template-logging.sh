ced7aed32a05f021eb337b4d31b5b1275cd8cc9b
Mayuresh <mskadu@users.noreply.github.com>
2017-02-17 16:44:26 +0000

    Initial Commit
    Migrating to PUBLIC GitHub

---
+++ template-logging.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Template shell script that provides logging functions
+# As usual error messages are logged to STDERRR so they can be rediverted as convenient.
+# Original Author: Mayuresh Kadu (Dt. 12/04/2001)
+
+# Keep the debug toggle flag at the top - alongside with the rest of your declarations
+#
+export DEBUG_FLAG=1	#0 = Off, 1=On 									# Turn me off if you dont want any INFO messages
+
+## Function to Log INFO level messages - to STDOUT
+##
+Log_Info() {
+	local timestamp=`date +"%Y%m%d%H%M%S"`
+	log_message="$timestamp INFO: $1"
+	test $DEBUG_FLAG -eq 1 && echo "$log_message"
+}
+
+## Function to Log ERROR level messages - to STDERR
+##
+Log_Error() {
+	local timestamp=`date +"%Y%m%d%H%M%S"`
+	log_message="$timestamp ERROR: $1"
+	echo "$log_message" 1>&2; 		# Error messages need to go to STDERR
+}
+
+# Example of Logging an error message
+Log_Error "This is an error message. Ideally I should be followed by a exit <non-zero-value>"
+
+# Example of a DEBUG/INFO message
+Log_Info "For your info, I am .. well .. a info message"
+
+# the end
