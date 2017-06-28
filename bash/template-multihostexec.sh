c90d739172fb7923885d2ee94e7b6945b389158c
Mayuresh Kadu <mskadu@gmail.com>
2017-06-28 15:20:29 +0100

    multihost scripts: Bug fix in script and extra comments in hosts files

--- bash/template-multihostexec.sh
+++ bash/template-multihostexec.sh
@@ -1,12 +1,14 @@
 #!bin/bash
 
-# Template Shell Script to connect to and execute commands on various hosts 
-# The list of hosts is contained in $MULTIHOSTEXEC_HOSTLIST_FILE 
-# Author: Mayuresh Kadu (Dt 12/04/2010)
+# Template Shell Script to connect to and execute commands on various hosts
+# The list of hosts is contained in $MULTIHOSTEXEC_HOSTLIST_FILE
+# Author: Mayuresh Kadu (Created: Dt 12/04/2010)
 #
 # Assumption: The same set of actions are to be repeated of each host
 #
-# TODO: display error/info messages using logging functions
+# TODO:
+# 	- display error/info messages using logging functions
+#		- display error message when the SSH to a host fails
 
 MULTIHOSTEXEC_HOSTLIST_FILE=template-multihost-hosts.lst
 
@@ -16,13 +18,13 @@ nHostListCount=`grep -v "^$\|#" $MULTIHOSTEXEC_HOSTLIST_FILE 2>/dev/null | wc -l
 arrHostList=`grep -v "^$\|#" $MULTIHOSTEXEC_HOSTLIST_FILE 2>/dev/null`
 
 
-if [ ! -s $MONITOR_HOSTLIST_FILE -o ! -r $MONITOR_HOSTLIST_FILE -o $nHostListCount -eq 0 ]; then
+if [ ! -s $MULTIHOSTEXEC_HOSTLIST_FILE -o ! -r $MULTIHOSTEXEC_HOSTLIST_FILE -o $nHostListCount -eq 0 ]; then
 	echo "Error: Cannot read host file or it is empty"
 	exit 1
 fi
 
 echo "Found $nHostListCount hosts"
-nHostIndex=0	
+nHostIndex=0
 for nHost in $arrHostList
 do
 	nHostIndex=`expr $nHostIndex + 1`
@@ -30,14 +32,15 @@ do
 	# source: http://stackoverflow.com/questions/305035/how-to-use-ssh-to-run-shell-script-on-a-remote-machine
 	ssh -q -T $nHost <<-'ENDSSH'
 
-  ## Sample set of actions - replace these with whatever you need done
+  ## Sample set of actions on the REMOTE host
   #
 	strTarFileName="$(hostname -f)-data-$(date +%Y%m%d).tar.gz"
 	echo "On REMOTE - Gathering data to $strTarFileName"
 	tar czf /some/remote-folder/$strTarFileName /some/other/folder/data/*.dat # && ls -l /some/remote-folder/$strTarFileName
 	test $? && echo "On REMOTE - Data gather complete"
 	ENDSSH
-	
+
+	# Sample set of actions on LOCAL host
   strRemoteFileName="/some/remote-folder/*$(date +%Y%m%d).tar.gz"
 	echo "On LOCAL - Downloading remote file $nHost:$strRemoteFileName $strRemoteFileName"
 	scp -q $nHost:$strRemoteFileName .
